c2b85d4062315e81c613a1c56a1502ec2e24dfb4
hyeonwoo Jeong <heywoo12@gmail.com>
2023-10-12 18:31:36 +0900

    feat : 초기 환경 세팅

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const morgan = require("morgan");
+
+dotenv.config();
+
+const { AppDataSource } = require("./src/models/data-sourece");
+const { routes } = require("./src/routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(routes);
+
+app.get("/", async (req, res) => {
+  try {
+    return res.status(200).json({ message: "Welecome" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.use((err, _, res, next) => {
+  const status = err.status || 500;
+  const errorMessage = err.message || "Internet Server Error";
+
+  return res.status(status).json({
+    error: `${status} ${errorMessage}`,
+  });
+});
+
+const start = async () => {
+  try {
+    await AppDataSource.initialize().then(() => console.log("Datasource initialized!"));
+    const port = process.env.SERVER_PORT;
+    app.listen(port, () => console.log(`Server is listening on '${port}'`));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
